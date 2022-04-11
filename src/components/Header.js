afa8963aa41577bb5cce256af493a65e7c966942
Stevens <stevens.sifuentes@unmsm.edu.pe>
2022-04-11 16:10:54 -0500

    .

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 const Header = ({ completedTasks, setCompletedTasks }) => {
   return (
     <header className='header'>
-      <h1 className='header__titulo'>Lista de Tareas</h1>
-      <button
+      <h1 className='header__titulo'>Has completado 0 de 0 todos</h1>
+      {/* <button
         className='header__boton'
         onClick={() => setCompletedTasks(!completedTasks)}>
           {completedTasks ? 'No mostrar completadas' : 'Mostrar completadas'}
         <FontAwesomeIcon 
           icon={completedTasks ? faEyeSlash : faEye} 
           className='header__icono-boton' /> 
-      </button>
+      </button> */}
     </header>
   )
 }
